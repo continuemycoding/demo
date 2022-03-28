4f24d579028f10bcefec8a3d0cb9af5b1091ee24
test <test@gmail.com>
2022-03-28 15:17:34 +0800

    default.header.static.js

--- http-proxy.js
+++ http-proxy.js
@@ -66,7 +66,7 @@ app.use('/', createProxyMiddleware({
         // if (req.path.includes("jquery.min.js"))
         //     return;
 
-        if (req.path.includes("require.static.js"))
+        if (req.path.includes("default.header.static.js"))
             return;
 
         const encoding = proxyRes.headers["content-encoding"];
