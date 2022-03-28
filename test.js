26a53185185d59752eaf42868ae92ae364efe75f
test <test@gmail.com>
2022-03-28 10:41:58 +0800

    x

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ const socks = require('socksv5');
 
 for (let i = 22222; i < 22333; i++) {
     let server = socks.createServer(function (info, accept, deny) {
-        winston.info(info.srcAddr, info.srcPort, info.dstAddr, info.dstPort);
+        console.log(info.srcAddr, info.srcPort, info.dstAddr, info.dstPort);
         accept();
     });
 
     server.listen(i, '0.0.0.0', function () {
-        winston.info('SOCKS server listening');
+        console.log('SOCKS server listening');
     });
 
     server.useAuth(socks.auth.None());
