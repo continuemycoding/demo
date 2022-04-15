c7722b37386f6fa6d337d88584243534a9afba32
test <test@gmail.com>
2022-04-15 11:30:19 +0800

    xxx

--- bot.ts
+++ bot.ts
@@ -186,32 +186,29 @@ bot.use((ctx, next) => {
 //     doSearch(nextPage.searchTerms, nextPage.startIndex, ctx);
 // });
 
-let verticalIndex = 0;
-
 bot.on('message', async (ctx) => {
 
     console.log(ctx.updateType);
 
     const text = ctx.message.text;
 
-     if(text)
-     {
-         if (text == "美女") {
+    if (text) {
+        if (text.includes("美女")) {
 
-             const vertical = (await axios.get(`https://service.picasso.adesk.com/v1/vertical/category/4e4d610cdf714d2966000000/vertical?skip=${20 * verticalIndex}`)).data.res.vertical;
+            const match = /美女 (\d+),\s(\d+)/gm.exec(text);
 
-             for (let i = 0; i < vertical.length; i++) {
-                 await ctx.replyWithPhoto({ url: vertical[i].img }, { caption: `第${verticalIndex * 20 + i + 1}张图` });
-             }
+            const vertical = (await axios.get(`https://service.picasso.adesk.com/v1/vertical/category/4e4d610cdf714d2966000000/vertical?skip=${match[1]}&limit=${match[2]}`)).data.res.vertical;
 
-             verticalIndex++;
-             return;
-         }
+            for (let i = 0; i < vertical.length; i++) {
+                await ctx.replyWithPhoto({ url: vertical[i].img }, { caption: `第${verticalIndex * 20 + i + 1}张图` });
+            }
+            return;
+        }
 
-         console.log(text, new Date(ctx.message.date * 1000).toLocaleString());
+        console.log(text, new Date(ctx.message.date * 1000).toLocaleString());
 
-         return;
-     }
+        return;
+    }
 
     // console.log(ctx.message);
 
