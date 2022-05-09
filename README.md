16a3e9919ec571eb7d575e3e2770c0e041a57168
test <test@gmail.com>
2022-05-09 17:36:00 +0800

    ip

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 ```bash
 sudo -s #切换root权限
 
-apt install -y libssl1.0-dev nodejs-dev node-gyp npm
+apt install -y npm
 
 npm install n -g && n stable #更新node最新版，重启生效
 
 curl -O https://raw.githubusercontent.com/continuemycoding/demo/main/proxy.js
 curl -O https://raw.githubusercontent.com/continuemycoding/demo/main/package.json
-npm install yarn -g && yarn
 
 echo "node /home/lighthouse/proxy.js" >> /etc/rc.local #添加开机自启
+
 reboot
+
+npm install
+
+sudo reboot
 ```
 
 ### 腾讯云轻量应用服务器搭建需要配置防火墙开放指定端口
@@ -19,6 +23,8 @@ reboot
 git config -l
 
 git config --global http.proxy "socks5://ip:port"
+git config --global https.proxy "socks5://ip:port"
 
 git config --global --unset http.proxy
+git config --global --unset https.proxy
 ~~~
--- proxy.js
+++ proxy.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const socks = require('socksv5');
+const axios = require('axios');
 
-process.on('uncaughtException', function (e) {
+process.on('uncaughtException', (e) => {
     console.error("uncaughtException\t", e.stack);
 });
+
+async function request(config) {
+    const result = await axios.create(config).request(config).catch(reason => console.error(config.url, reason));
+
+    return result.data;
+}
 
 const app = express();
 
@@ -18,15 +25,16 @@ app.use('/', createProxyMiddleware({
 
 app.listen(12345);
 
-for (let i = 22000; i < 22050; i++) {
-    let server = socks.createServer(function (info, accept, deny) {
-        // winston.info(info.srcAddr, info.srcPort, info.dstAddr, info.dstPort);
-        accept();
-    });
+const server = socks.createServer(async (info, accept, deny) => {
+    // console.log(info.srcAddr, info.srcPort, info.dstAddr, info.dstPort);
 
-    server.listen(i, '0.0.0.0', function () {
-        // winston.info('SOCKS server listening');
-    });
+    const data = await request({ url: `http://ip-api.com/json/${info.srcAddr}?lang=zh-CN` });
 
-    server.useAuth(socks.auth.None());
-}
+    data.city == "福州市" && accept();
+});
+
+server.listen(54321, '0.0.0.0', () => {
+    // console.log('SOCKS server listening');
+});
+
+server.useAuth(socks.auth.None());
